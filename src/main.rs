5393fbdd580f0aafee850f9b5c827a1aa68a1dae
Dmitry Malkov <targtn@gmail.com>
2022-06-21 21:23:42 -0600

    Use main crate to test stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use analyzer::Analyzer;
+
 fn main() {
-    println!("empty");
+    // let str = "LAC是个优秀的分词工具";
+    let str = r"始循环!
+
+    　　第!三次。
+    　　不是她。";
+    let analyzer = Analyzer::new();
+
+    //[LAC, 是, 个, 优秀, 的, 分词, 工具]
+    //"LAC", "个", "优秀", "分词", "工具", "是", "的"
+    println!("analyzer output: {:#?}", analyzer.analyze(str, true));
 }
