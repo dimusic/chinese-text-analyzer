8647a55b5522bff6d8a0b2a5dcd7b01169f60c7c
Dmitry Malkov <dmitry.malkov@live.com>
2022-12-31 21:57:09 -0600

    Remove unused tauri version of the page

--- web-app/src/app.tsx
+++ web-app/src/app.tsx
@@ -1,14 +1,11 @@
 import TextAnalyzerWasmPage from "./pages/text-analyzer/text-analyzer-wasm.page";
-import TextAnalyzerPage from "./pages/text-analyzer/text-analyzer.page";
 import "antd/dist/antd.css";
 import "./app.css";
-
-const IS_TAURI: boolean = typeof window.__TAURI_IPC__ !== "undefined";
 
 function App() {
     return (
         <div className="app">
-            {IS_TAURI ? <TextAnalyzerPage></TextAnalyzerPage> : <TextAnalyzerWasmPage></TextAnalyzerWasmPage>}
+            <TextAnalyzerWasmPage></TextAnalyzerWasmPage>
         </div>
     );
 }
