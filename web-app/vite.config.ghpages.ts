96e4575e099bbf3274100f1818ced79fda94eb81
Dmitry Malkov <targtn@gmail.com>
2023-01-01 15:57:24 -0600

    Update npm dependencies

--- web-app/vite.config.ghpages.ts
+++ web-app/vite.config.ghpages.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/chinese-text-analyzer/'
-})
+    plugins: [react()],
+    base: "/chinese-text-analyzer/",
+});
